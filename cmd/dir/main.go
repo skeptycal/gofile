9e9bdab63606d7be4e858a19adc8d7b6241c575e
Michael Treanor <26148512+skeptycal@users.noreply.github.com>
2021-04-19 19:09:23 -0500

    examples

--- cmd/dir/main.go
+++ cmd/dir/main.go
@@ -1,29 +1,22 @@
 package main
 
-import (
-	"fmt"
+// func Dir(path string) string {
+// 	command := fmt.Sprintf("ls -R %s", path)
+// 	result := zsh.Sh(command)
+// 	return result
+// }
 
-	log "github.com/sirupsen/logrus"
-	"github.com/skeptycal/zsh"
-)
+// func DirTime(args string) string {
+// 	command := fmt.Sprintf("time ls -R . %s", args)
+// 	result := zsh.Sh(command)
+// 	return result
+// }
 
-func Dir(path string) string {
-	command := fmt.Sprintf("ls -R %s", path)
-	result := zsh.Sh(command)
-	return result
-}
+// func main() {
 
-func DirTime(args string) string {
-	command := fmt.Sprintf("time ls -R . %s", args)
-	result := zsh.Sh(command)
-	return result
-}
-
-func main() {
-
-	testpath := "/Users/skeptycal/local_coding"
-	fmt.Println("Directory Listing Benchmarks:\n ")
-	log.Info("logger started")
-	fmt.Println("")
-	fmt.Println(Dir(testpath))
-}
+// 	testpath := "/Users/skeptycal/local_coding"
+// 	fmt.Println("Directory Listing Benchmarks:\n ")
+// 	log.Info("logger started")
+// 	fmt.Println("")
+// 	fmt.Println(Dir(testpath))
+// }
--- cmd/dir/main_test.go
+++ cmd/dir/main_test.go
@@ -1,65 +1,61 @@
 package main
 
-import (
-	"testing"
-)
+// func BenchmarkDirShellBM(b *testing.B) {
 
-func BenchmarkDirShellBM(b *testing.B) {
+// }
 
-}
+// func Test_main(t *testing.T) {
+// 	tests := []struct {
+// 		name string
+// 	}{
+// 		// TODO: Add test cases.
+// 		{"main"},
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			main()
+// 		})
+// 	}
+// }
 
-func Test_main(t *testing.T) {
-	tests := []struct {
-		name string
-	}{
-		// TODO: Add test cases.
-		{"main"},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			main()
-		})
-	}
-}
+// func TestDir(t *testing.T) {
+// 	type args struct {
+// 		path string
+// 	}
+// 	tests := []struct {
+// 		name string
+// 		args args
+// 		want string
+// 	}{
+// 		// TODO: Add test cases.
+// 		{"pwd", args{"."}, "main.go\nmain_test.go\n"},
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			if got := Dir(tt.args.path); got != tt.want {
+// 				t.Errorf("Dir() = %v, want %v", got, tt.want)
+// 			}
+// 		})
+// 	}
+// }
 
-func TestDir(t *testing.T) {
-	type args struct {
-		path string
-	}
-	tests := []struct {
-		name string
-		args args
-		want string
-	}{
-		// TODO: Add test cases.
-		{"pwd", args{"."}, "main.go\nmain_test.go\n"},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := Dir(tt.args.path); got != tt.want {
-				t.Errorf("Dir() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestDirTime(t *testing.T) {
-	type args struct {
-		args string
-	}
-	tests := []struct {
-		name string
-		args args
-		want string
-	}{
-		// TODO: Add test cases.
-        {"test", args{""}, "main.go\nmain_test.go\n"},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := DirTime(tt.args.args); got != tt.want {
-				t.Errorf("DirTime() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
+// func TestDirTime(t *testing.T) {
+// 	type args struct {
+// 		args string
+// 	}
+// 	tests := []struct {
+// 		name string
+// 		args args
+// 		want string
+// 	}{
+// 		// TODO: Add test cases.
+//         {"test", args{""}, "main.go\nmain_test.go\n"},
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			if got := DirTime(tt.args.args); got != tt.want {
+// 				t.Errorf("DirTime() = %v, want %v", got, tt.want)
+// 			}
+// 		})
+// 	}
+// }
