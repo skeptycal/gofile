aafdf7f109f457ee2ceb44e9e8980d68b435c9d4
skeptycal <26148512+skeptycal@users.noreply.github.com>
2021-05-21 09:01:47 -0500

    move textfile out to package

---
+++ textfile/textfile.go
@@ -0,0 +1,51 @@
+package textfile
+
+import (
+	"bufio"
+	"os"
+
+	"github.com/skeptycal/gofile/basicfile"
+)
+
+const (
+	PathSep = os.PathSeparator
+	ListSep = os.PathListSeparator
+	NewLine = '\n'
+)
+
+type TextFile interface {
+	basicfile.BasicFile
+	Text() string
+	Lines() (retval []string, err error)
+	Sep(c byte)
+}
+
+// textfile is a file type that is specialized for utf-8 text
+type textfile struct {
+	basicfile.Basicfile
+	linesep   byte
+	recordsep byte
+}
+
+func (d *textfile) Sep(c byte) {
+	d.linesep = c
+}
+
+func (d *textfile) RecordSep(c byte) {
+	d.recordsep = c
+}
+
+func (d *textfile) Text() string {
+	return string(d.Data())
+}
+
+func (d *textfile) Lines() (retval []string, err error) {
+
+	scanner := bufio.NewScanner(d)
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		retval = append(retval, scanner.Text())
+	}
+
+	return retval, scanner.Err()
+}
