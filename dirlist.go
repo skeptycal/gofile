f0f7b4fe78c061dad3cd1aa4da70aa548651991a
Michael Treanor <skeptycal@gmail.com>
2022-03-30 03:44:22 -0500

    modified API

--- dirlist.go
+++ dirlist.go
@@ -2,10 +2,8 @@ package gofile
 
 import (
 	"io/fs"
-	"io/ioutil"
 	"os"
 	"path/filepath"
-	"time"
 
 	"github.com/skeptycal/basicfile"
 )
@@ -27,11 +25,11 @@ type (
 // type datafile =
 
 type dirList struct {
-	providedName string
-	pathname     string
-	count        int
-	opts         dirOptions
-	list         []BasicFile // []DataFile // fs.FileInfo
+	providedName string      // original name provided
+	name         string      // JIT absolute file name
+	count        int         // JIT cached file count
+	opts         dirOptions  // options for directory listing
+	list         []BasicFile // or []DataFile // fs.FileInfo
 }
 
 func (l *dirList) Len() int {
@@ -41,45 +39,44 @@ func (l *dirList) Len() int {
 	return l.count
 }
 
-func (l *dirList) Dirs() (list []fs.FileInfo) {
+// Dirs returns a list of all objects
+// in the dirList that are directories.
+func (l *dirList) Dirs() []fs.FileInfo {
+	list := make([]fs.FileInfo, 0, l.Len())
+
 	for _, f := range l.list {
 		if f.IsDir() {
 			list = append(list, f)
 		}
 	}
-	return
+	return list
 }
 
 func (l *dirList) Path() string {
-	if l.pathname == "" {
+	if l.name == "" {
 		if !IsDir(l.providedName) {
-			log.Errorf("directory %s not found", l.providedName)
+			log.Errorf("%s is not a directory", l.providedName)
 			return ""
 		}
-		dir, err := filepath.Abs(l.providedName)
+		var err error
+		l.name, err = filepath.Abs(l.providedName)
 		if err != nil {
 			log.Error(err)
 			return ""
 		}
-		l.pathname = dir
 	}
-	return l.pathname
+	return l.name
 }
 
-func fi2bf(fi fs.FileInfo) (BasicFile, error) {
-	f, err := os.Open(fi.Name())
-
-	if err != nil {
-		return nil, err
+func (l *dirList) Abs() string {
+	if l.name == "" {
+		l.name, _ = filepath.Abs(l.providedName)
 	}
-
-	return &basicFile{
-		File:    f,
-		fi:      fi,
-		modTime: time.Now(),
-		lock:    false,
-	}, nil
+	return l.name
 }
+
+func (l *dirList) Dir() string  { return filepath.Dir(l.Abs()) }
+func (l *dirList) Base() string { return filepath.Base(l.Abs()) }
 
 // Returns the list of files in the directory.
 //
@@ -87,12 +84,17 @@ func fi2bf(fi fs.FileInfo) (BasicFile, error) {
 // skipped and processing will continue.
 func (l *dirList) List() (fi []BasicFile, err error) {
 	if l.Len() == 0 {
-		list, err := ioutil.ReadDir(l.Path())
+
+		// path := l.Dir()
+
+		list, err := os.ReadDir(l.Path())
+		// list, err := ioutil.ReadDir(l.Path())
 		if err != nil {
 			return nil, err
 		}
-		for _, file := range list {
-			bf, err := fi2bf(file)
+		for _, dir := range list {
+			dir.Name()
+			bf, err := basicfile.NewBasicFile(dir.Name())
 			if err != nil {
 				continue
 			}
